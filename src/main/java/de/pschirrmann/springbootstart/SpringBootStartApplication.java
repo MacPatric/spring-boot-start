ad676c70a67d23c92c40ef27d445cd518b774861
Patric Schirrmann <patric.schirrmann@pschirrmann.de>
2019-11-09 15:30:35 +0100

    Adds persistence and security.

--- src/main/java/de/pschirrmann/springbootstart/SpringBootStartApplication.java
+++ src/main/java/de/pschirrmann/springbootstart/SpringBootStartApplication.java
@@ -2,7 +2,11 @@ package de.pschirrmann.springbootstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("de.pschirrmann.springbootstart.persistence.repo") 
+@EntityScan("de.pschirrmann.springbootstart.persistence.model")
 @SpringBootApplication
 public class SpringBootStartApplication {
 
